0f44669adce6d372818677d559ea588d3b9a4a2f
Nerd <hndppt@outlook.com>
2023-02-16 23:09:36 +0800

    reback

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,6 +3,6 @@ import { defineComponent } from "vue";
 export default defineComponent({
     
     setup() {
-        return () => <div >Nerd</div>;
+        return () => <div>Home</div>;
     },
 });
